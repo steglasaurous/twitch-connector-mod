322e2934c62a1526473c9626098a83d84930fd6c
steglasaurous <me@steglasaurous.com>
2023-03-25 22:35:45 -0400

    Version bump

--- TwitchConnectorMod/Properties/AssemblyInfo.cs
+++ TwitchConnectorMod/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using TwitchConnectorMod;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
-[assembly: MelonInfo(typeof(TwitchConnectorMod.TwitchConnectorMod), "Twitch Connector Mod", "1.0.0", "Steglasaurous")]
+[assembly: MelonInfo(typeof(TwitchConnectorMod.TwitchConnectorMod), "Twitch Connector Mod", "1.1.0", "Steglasaurous")]
 [assembly: MelonGame()] // FIXME: Confirm this might indeed work anywhere - maybe useful for other supported games? 
